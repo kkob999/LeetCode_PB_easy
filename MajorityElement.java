87c47cf16df7b462dc0988b807f26abd18aa958b
KKob <68890159+kkob999@users.noreply.github.com>
2023-02-02 04:07:36 +0700

    Finish
    O(n)^2

--- MajorityElement.java
+++ MajorityElement.java
@@ -13,3 +13,20 @@ Example 2:
 Input: nums = [2,2,1,1,1,2,2]
 Output: 2
 */
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        for (int i = 0; i < nums.length; i++){
+            int count = 0;
+            for (int j = 0; j < nums.length; j++){
+                if (nums[j]==nums[i]){
+                    count += 1;
+                }
+            }
+            if (count > nums.length/2){
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+}
