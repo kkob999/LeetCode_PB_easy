f5d1890647491ab6ce5814c99930267526d4935a
KKob <68890159+kkob999@users.noreply.github.com>
2023-02-03 03:24:14 +0700

    Create minimum-cost-of-buying-candies-with-discount.java

---
+++ minimum-cost-of-buying-candies-with-discount.java
@@ -0,0 +1,33 @@
+/*A shop is selling candies at a discount. For every two candies sold, the shop gives a third candy for free.
+
+The customer can choose any candy to take away for free as long as the cost of the chosen candy is less than or equal to the minimum cost of the two candies bought.
+
+For example, if there are 4 candies with costs 1, 2, 3, and 4, and the customer buys candies with costs 2 and 3, they can take the candy with cost 1 for free, but not the candy with cost 4.
+Given a 0-indexed integer array cost, where cost[i] denotes the cost of the ith candy, return the minimum cost of buying all the candies.
+
+Input: cost = [6,5,7,9,2,2]
+Output: 23
+Explanation: The way in which we can get the minimum cost is described below:
+- Buy candies with costs 9 and 7
+- Take the candy with cost 6 for free
+- We buy candies with costs 5 and 2
+- Take the last remaining candy with cost 2 for free
+Hence, the minimum cost to buy all candies is 9 + 7 + 5 + 2 = 23.
+*/
+
+
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int sum = 0;
+        int count = 0;
+        for(int i = cost.length; i > 0; i--){
+            sum += cost[i-1];
+            count += 1;
+            if (count%3==0){
+                sum -= cost[i-1];
+            }
+        }
+        return sum;
+    }
+}
